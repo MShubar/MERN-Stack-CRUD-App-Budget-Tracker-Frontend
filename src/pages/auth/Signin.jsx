a8774f6bf945a223721f9820fbaf61c371da7d84
MShubar <pokeeeeepow@gmail.com>
2025-01-07 17:44:58 +0300

    sign in

--- src/pages/auth/Signin.jsx
+++ src/pages/auth/Signin.jsx
@@ -24,7 +24,7 @@ const Signin = ({ onLogin }) => {
       const token = response.data.token
       localStorage.setItem('token', token)
       onLogin()
-      navigate('/transactionlist')
+      navigate('/transaction/list')
 
       setFormData(initialFormData)
     } catch (error) {
