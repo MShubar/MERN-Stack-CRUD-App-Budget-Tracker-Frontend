1abe737a3537c16c7679c60d2dadb1f3704ac3a0
MShubar <pokeeeeepow@gmail.com>
2025-01-09 18:58:14 +0300

    Alot of features

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 import { PieChart } from '@mui/x-charts/PieChart'
 import { BarChart } from '@mui/x-charts/BarChart'
-import { Grid, Paper, Typography, Box } from '@mui/material'
+import {
+  Grid,
+  Paper,
+  Typography,
+  Box,
+  List,
+  ListItem,
+  ListItemText
+} from '@mui/material'
 
 const getMonthlyTransactionData = (transactions) => {
   const monthlyData = Array(12).fill(0)
@@ -47,74 +56,191 @@ const Dashboard = ({ transactions }) => {
     getMonthlyTransactionData(transactions)
   const categoryDistribution = getCategoryDistribution(transactions)
 
+  const sortedTransactions = [...transactions].sort(
+    (a, b) => new Date(b.date) - new Date(a.date)
+  )
+
+  // State to track when each section is in view
+  const [inView, setInView] = useState({
+    pieChart: false,
+    barChart: false,
+    transactionList: false
+  })
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const id = entry.target.id
+          if (entry.isIntersecting) {
+            setInView((prevState) => ({
+              ...prevState,
+              [id]: true
+            }))
+          }
+        })
+      },
+      { threshold: 0.5 } // Trigger when 50% of the component is visible
+    )
+
+    // Observe each component
+    document.querySelectorAll('.section').forEach((section) => {
+      observer.observe(section)
+    })
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [])
+
   return (
     <Box sx={{ padding: 3, minHeight: '100vh' }}>
-      <Typography variant="h3" component="h1" gutterBottom>
+      <Typography variant="h5" component="h2" gutterBottom>
         Dashboard
       </Typography>
 
       <Grid container spacing={4} alignItems="flex-start">
         <Grid item xs={12} sm={6}>
-          <Paper sx={{ padding: 2, borderRadius: 2, boxShadow: 3 }}>
-            <Typography variant="h5" component="h2" gutterBottom>
-              Category Distribution
-            </Typography>
-            <PieChart
-              series={[
-                {
-                  data: categoryDistribution,
-                  highlightScope: { fade: 'global', highlight: 'item' },
-                  faded: {
-                    innerRadius: 30,
-                    additionalRadius: -30,
-                    color: 'gray'
-                  }
-                }
-              ]}
-              height={300}
-              style={{ borderRadius: '8px' }}
-            />
+          <Paper
+            sx={{ padding: 2, borderRadius: 2, boxShadow: 3 }}
+            className="section"
+            id="pieChart"
+          >
+            {inView.pieChart && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+              >
+                <Typography variant="h5" component="h2" gutterBottom>
+                  Category Distribution
+                </Typography>
+                <PieChart
+                  series={[
+                    {
+                      data: categoryDistribution,
+                      highlightScope: { fade: 'global', highlight: 'item' },
+                      faded: {
+                        innerRadius: 30,
+                        additionalRadius: -30,
+                        color: 'gray'
+                      }
+                    }
+                  ]}
+                  height={400}
+                  style={{ borderRadius: '8px' }}
+                />
+              </motion.div>
+            )}
           </Paper>
         </Grid>
 
         <Grid item xs={12} sm={6}>
-          <Paper sx={{ padding: 2, borderRadius: 2, boxShadow: 3 }}>
-            <Typography variant="h5" component="h2" gutterBottom>
-              Monthly Net Balance
-            </Typography>
-            <BarChart
-              xAxis={[
-                {
-                  scaleType: 'band',
-                  data: [
-                    'Jan',
-                    'Feb',
-                    'Mar',
-                    'Apr',
-                    'May',
-                    'Jun',
-                    'Jul',
-                    'Aug',
-                    'Sep',
-                    'Oct',
-                    'Nov',
-                    'Dec'
-                  ]
-                }
-              ]}
-              series={[
-                {
-                  data: monthlyData,
-                  label: 'Monthly Balance'
-                }
-              ]}
-              width={500}
-              height={300}
-              style={{ borderRadius: '8px' }}
-            />
+          <Paper
+            sx={{ padding: 2, borderRadius: 2, boxShadow: 3 }}
+            className="section"
+            id="barChart"
+          >
+            {inView.barChart && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+              >
+                <Typography variant="h5" component="h2" gutterBottom>
+                  Monthly Net Balance
+                </Typography>
+                <BarChart
+                  xAxis={[
+                    {
+                      scaleType: 'band',
+                      data: [
+                        'Jan',
+                        'Feb',
+                        'Mar',
+                        'Apr',
+                        'May',
+                        'Jun',
+                        'Jul',
+                        'Aug',
+                        'Sep',
+                        'Oct',
+                        'Nov',
+                        'Dec'
+                      ]
+                    }
+                  ]}
+                  series={[
+                    {
+                      data: monthlyData,
+                      label: 'Monthly Balance'
+                    }
+                  ]}
+                  width={500}
+                  height={400}
+                  style={{ borderRadius: '8px' }}
+                />
+              </motion.div>
+            )}
           </Paper>
         </Grid>
       </Grid>
+
+      <Typography variant="h5" component="h2" gutterBottom>
+        Latest Transactions
+      </Typography>
+      <Paper
+        sx={{ padding: 2, borderRadius: 2, boxShadow: 3 }}
+        className="section"
+        id="transactionList"
+      >
+        {inView.transactionList && (
+          <Box
+            sx={{
+              maxHeight: '400px',
+              overflowY: 'auto',
+              paddingRight: 1
+            }}
+          >
+            <List>
+              {sortedTransactions.map((transaction, index) => (
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, translateY: 20 }}
+                  animate={{ opacity: 1, translateY: 0 }}
+                  transition={{ duration: 3, delay: index * 0.1 }}
+                >
+                  <ListItem>
+                    <ListItemText
+                      primary={
+                        <>
+                          <Typography
+                            component="span"
+                            sx={{
+                              fontWeight: 'bold',
+                              color:
+                                transaction.type === 'Debit' ? 'red' : 'green'
+                            }}
+                          >
+                            {transaction.type === 'Debit' ? '-' : '+'}{' '}
+                            {transaction.amount}
+                          </Typography>{' '}
+                          <Typography component="span" sx={{ color: 'gray' }}>
+                            (Category: {transaction.name})
+                          </Typography>
+                        </>
+                      }
+                      secondary={`Date: ${new Date(
+                        transaction.date
+                      ).toLocaleDateString()}`}
+                    />
+                  </ListItem>
+                </motion.div>
+              ))}
+            </List>
+          </Box>
+        )}
+      </Paper>
     </Box>
   )
 }
