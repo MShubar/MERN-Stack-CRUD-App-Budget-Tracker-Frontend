452cc13cd41169629a3f2bc32be3222d60621ef9
MShubar <pokeeeeepow@gmail.com>
2025-01-10 03:36:40 +0300

    Dashboard done

--- src/pages/category/CategoryDetails.jsx
+++ src/pages/category/CategoryDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams, Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export const CategoryDetails = ({ categories, transactions }) => {
   let navigate = useNavigate()
